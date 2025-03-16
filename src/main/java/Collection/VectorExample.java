ad815316d0899898e8759a53eb731ff5fa790c01
sanjar-qosim <sanjar171766@gmail.com>
2025-03-17 03:52:31 +0500

    Vector

---
+++ src/main/java/Collection/VectorExample.java
@@ -0,0 +1,17 @@
+package Collection;
+
+import java.util.Vector;
+
+public class VectorExample {
+
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+        vector.add("Zaur");
+        vector.add("Misha");
+        vector.add("Oleg");
+        vector.add("Katya");
+        System.out.println(vector);
+        System.out.println(vector.firstElement());
+        System.out.println(vector.lastElement());
+    }
+}
