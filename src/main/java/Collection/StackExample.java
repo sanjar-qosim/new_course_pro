ed56c08af3133ed74750194b60cc03d69e21d0c9
sanjar-qosim <sanjar171766@gmail.com>
2025-03-17 22:04:34 +0500

    Stack

---
+++ src/main/java/Collection/StackExample.java
@@ -0,0 +1,27 @@
+package Collection;
+
+public class StackExample {
+
+    static void abc1(){
+        System.out.println("abc1 starts");
+        System.out.println("abc1 ends");
+    }
+
+    static void abc2(){
+        System.out.println("abc2 starts");
+        abc1();
+        System.out.println("abc2 ends");
+    }
+
+    static void abc3(){
+        System.out.println("abc3 starts");
+        abc2();
+        System.out.println("abc3 ends");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main starts");
+        abc3();
+        System.out.println("Main ends");
+    }
+}
---
+++ src/main/java/Collection/map_interface/HashMapEx1.java
@@ -0,0 +1,36 @@
+package Collection.map_interface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEx1 {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map1 = new HashMap<>();
+
+        map1.put(1000, "Zaur Tregulov");
+        map1.put(3568, "Ivan Ivanov");
+        map1.put(6578, "Katya Sidorova");
+        map1.put(15879, "Nikolay Petrov");
+        map1.put(98745, "Nikolay Petrov");
+        map1.put(null, null);
+        map1.putIfAbsent(15879, "Nikolay Petrov");
+
+        System.out.println(map1);
+
+        System.out.println(map1.get(1000));
+
+        map1.remove(15879);
+        System.out.println(map1);
+
+        System.out.println(map1.containsValue("Katya Sidorova"));
+        System.out.println(map1.containsKey(6578));
+
+        System.out.println(map1.keySet());
+
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("Sasha", "Beliy");
+        map2.put("Misha", "Umniy");
+
+    }
+}
