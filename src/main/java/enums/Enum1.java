c0297db8a2db3d8dba4ea427eea9b547c8de7a46
sanjar-qosim <sanjar171766@gmail.com>
2025-03-30 18:02:32 +0500

    Start enum

---
+++ src/main/java/enums/Enum1.java
@@ -0,0 +1,18 @@
+package enums;
+
+public enum Enum1 {
+
+}
+
+enum WeekDays {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+}
+class Today {
+    WeekDays weekDays;
+
+}
---
+++ src/main/java/enums/Test1.java
@@ -0,0 +1,8 @@
+package enums;
+
+public class Test1 {
+
+    void method(String dayOfWeek) {
+        System.out.println("Today is: " + dayOfWeek);
+    }
+}
