609bbc17427017a27e7823c7b3e05cf7e7979883
sanjar-qosim <sanjar171766@gmail.com>
2025-04-04 21:54:03 +0500

    FileReader FileWriter

---
+++ src/main/java/work_with_files/FileReaderEx.java
@@ -0,0 +1,26 @@
+package work_with_files;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderEx {
+
+    public static void main(String[] args) throws IOException {
+        FileReader reader = null;
+
+        try {
+            reader = new FileReader("src\\main\\java\\work_with_files\\test2.txt");
+            int character;
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
+            }
+            System.out.println();
+            System.out.println("Done!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            reader.close();
+        }
+    }
+}
---
+++ src/main/java/work_with_files/FileWriterEx.java
@@ -0,0 +1,32 @@
+package work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEx {
+
+    public static void main(String[] args) throws IOException {
+        String rubai = "Много лет размышлял я над жизнью земной.\n" +
+                "Непонятного нет для меня под луной.\n" +
+                "Мне известно, что мне ничего не известно! —\n" +
+                "Вот последняя правда, открытая мной.\n";
+
+        String s = "Hello";
+
+        FileWriter writer = null;
+
+        try {
+//            writer = new FileWriter("C:\\Users\\skasimov\\Desktop\\new_course_pro-main\\src\\main\\java\\work_with_files\\test1.txt");
+            writer = new FileWriter("src\\main\\java\\work_with_files\\test2.txt");
+            for(int i = 0; i < rubai.length(); i ++) {
+                writer.write(rubai.charAt(i));
+            }
+//            writer.write(s);
+            System.out.println("Done!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            writer.close();
+        }
+    }
+}
