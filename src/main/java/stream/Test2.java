4c534ff218c713a407af2795b9e62294a661f9a9
sanjar-qosim <sanjar171766@gmail.com>
2025-04-17 09:01:20 +0500

    Stream map

---
+++ src/main/java/stream/Test2.java
@@ -0,0 +1,99 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 'm', 22, 3, 8.3);
+        Student st2 = new Student("Nikolay", 'm', 28, 2, 6.4);
+        Student st3 = new Student("Elena", 'f', 19, 1, 8.9);
+        Student st4 = new Student("Petr", 'm', 35, 4, 7);
+        Student st5 = new Student("Durov", 'm', 23, 3, 7.4);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(st1);
+        studentList.add(st2);
+        studentList.add(st3);
+        studentList.add(st4);
+        studentList.add(st5);
+
+        System.out.println(studentList);
+
+        studentList = studentList.stream().filter(
+                element -> element.getAge() > 22 && element.getAvgGrade() < 7.2
+        ).collect(Collectors.toList());
+
+        System.out.println(studentList);
+    }
+}
+
+class Student {
+
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public double getAvgGrade() {
+        return avgGrade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setAvgGrade(double avgGrade) {
+        this.avgGrade = avgGrade;
+    }
+}
