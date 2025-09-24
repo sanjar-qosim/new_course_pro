aa2bb243dc443f33e1e464a422515fe23fb3deb7
Sanjar <sanjar171766@gmail.com>
2025-09-24 23:12:38 +0500

    Stream(concat and distinct)

---
+++ src/main/java/stream/Test6.java
@@ -0,0 +1,25 @@
+package stream;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Test6 {
+
+    public static void main(String[] args) {
+        Stream<Integer> stream1 = Stream.of(1,2,3,3,4,2);
+        stream1.filter(el -> {
+            System.out.println("!!!");
+            return el %2 == 0;
+        }).collect(Collectors.toList());
+
+
+        Stream<Integer> stream2 = Stream.of(1, 2, 3, 4, 5);
+        Stream<Integer> stream3 = Stream.of(6, 7, 8, 9, 10);
+
+        Stream<Integer> stream4 = Stream.concat(stream2, stream3);
+        stream4.forEach(System.out::println);
+
+        Stream<Integer> stream5 = Stream.of(1, 2, 3, 4, 5, 1, 2, 3);
+        stream5.distinct().forEach(System.out::println);
+    }
+}
